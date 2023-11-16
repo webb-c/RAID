75ede09a10331dc5dbdf32386cff542ab79f543b
vaughn <jwst0208@gmail.com>
2023-11-17 03:51:01 +0900

    add: basic train code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import warnings
 import argparse
+from tqdm import tqdm
+from pyprnt import prnt
 from Agent import Agent
 from Environment import Env
 
@@ -24,9 +26,9 @@ def parse_opt(known=False):
     parser.add_argument("-lmbda", "--lmbda", type=float, default=0.9, help="hyperparameter lambda for cal GAE")
     parser.add_argument("-clip", "--eps_clip", type=float, default=0.2, help="clip parameter for PPO")
     
-    parser.add_argument("-epoch", "--num_epoch", type=int, default=10, help="number of Total train episode")
-    parser.add_argument("-step", "--num_step", type=int, default=50, help="number of PPO's step" )
-    parser.add_argument("-rollout", "--rollout_len", type=int, default=3, help="rollout length")
+    parser.add_argument("-episode", "--num_episode", type=int, default=1000, help="number of train episode")
+    parser.add_argument("-epoch", "--num_epoch", type=int, default=10, help="number of maximum action")
+    parser.add_argument("-step", "--num_step", type=int, default=10, help="number of PPO's step" )
     parser.add_argument("-Kepoch", "--K_epochs", type=int, default=3, help="update policy for K Epoch")
     parser.add_argument("-buff", "--buffer_size", type=int, default=10, help="buffer size")
     parser.add_argument("-batch", "--minibatch_size", type=int, default=32, help="minibatch size")
@@ -38,13 +40,44 @@ def parse_opt(known=False):
     
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
-
+#TODO Train / Test 모드 전환
 def main(conf):
-    print(conf)
+    print("===== Experiment Setting =====")
+    prnt(conf)
     print()
     
+    # Hyper-parameter
+    num_episode = conf["num_episode"]
+    num_step = conf["num_step"] 
+    interval = 20
+    
+    # Env, Agent setting
+    env = Env(conf)
+    env.train()
+    agent = Agent(conf)
+    
+    # Train code
+    for episode in tqdm(range(num_episode)):
+        total_reward = 0
+        state, _ = env.reset()
+        done = False
+        for step in range(num_step):
+            actions, action_probs = agent.get_actions(state)
+            state_prime, reward, terminated, truncated, info = env.step(actions)
+            if terminated or truncated :
+                done = True
+            agent.put_data((state, actions, reward, state_prime, action_probs, done))
+            total_reward += reward
+            state = state_prime
+            if done : 
+                break
+        agent.train_net()
+        
+        if episode % interval == 0 :
+            print("# of episode :{}, avg reward : {:.2f}, total reward : {:.2f}".format(episode, total_reward/step, total_reward))
     
 if __name__ == "__main__":
+    warnings.filterwarnings('ignore', category=UserWarning)
     args = parse_opt()
     conf = dict(**args.__dict__)
     main(conf)
